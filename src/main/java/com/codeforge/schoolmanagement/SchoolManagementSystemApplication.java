18c6437079a982c843c35443362aa4e341dd39cc
Rafael Moreira <13rjmms@gmail.com>
2024-09-16 15:09:53 +0100

    ADD: Teacher Model

--- src/main/java/com/codeforge/schoolmanagement/SchoolManagementSystemApplication.java
+++ src/main/java/com/codeforge/schoolmanagement/SchoolManagementSystemApplication.java
@@ -1,6 +1,10 @@
 package com.codeforge.schoolmanagement;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.ZoneId;
 import java.util.Arrays;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,8 +12,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.codeforge.schoolmanagement.model.Role;
+import com.codeforge.schoolmanagement.model.Teacher;
 import com.codeforge.schoolmanagement.model.User;
 import com.codeforge.schoolmanagement.repository.RoleRepository;
+import com.codeforge.schoolmanagement.repository.TeacherRepository;
 import com.codeforge.schoolmanagement.repository.UserRepository;
 
 @SpringBootApplication
@@ -19,6 +25,8 @@ public class SchoolManagementSystemApplication implements CommandLineRunner{
 	private UserRepository userRepo;
 	@Autowired
 	private RoleRepository roleRepo;
+	@Autowired
+	private TeacherRepository teacherRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolManagementSystemApplication.class, args);
@@ -32,9 +40,16 @@ public class SchoolManagementSystemApplication implements CommandLineRunner{
 		User u1 = new User(null, "admin@gmail.com", "batata", true, r1);
 		User u2 = new User(null, "student@gamilc.com", "limão", true, r2);
 		
+		LocalDate birthdate = LocalDate.of(1990, Month.JANUARY, 15);
+
+		// Convert LocalDate to Date
+		Date dateOfBirth = Date.from(birthdate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		
+		Teacher t1 = new Teacher(null, "Rafael", dateOfBirth, "Rua S.João N13", "Portuguese", "Male", "", "244173087", "15715842", "", "");
+		
 		roleRepo.saveAll(Arrays.asList(r1, r2));
 		userRepo.saveAll(Arrays.asList(u1, u2));
-		
+		teacherRepo.saveAll(Arrays.asList(t1));
 	}
 
 }
---
+++ src/main/java/com/codeforge/schoolmanagement/model/Teacher.java
@@ -0,0 +1,159 @@
+package com.codeforge.schoolmanagement.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Teachers")
+public class Teacher implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private Date birthDate;
+	private String address;
+	private String nationality;
+	private String gender;
+	private String observation;
+	private String taxIdentificationNumber;
+	private String identityCardNumber;
+	private String registrationNumber;
+	private String photoPath;
+	
+	public Teacher() {
+	}
+	
+	public Teacher(Integer id, String name, Date birthDate, String address, String nationality, String gender,
+			String observation, String taxIdentificationNumber, String identityCardNumber, String registrationNumber,
+			String photoPath) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+		this.address = address;
+		this.nationality = nationality;
+		this.gender = gender;
+		this.observation = observation;
+		this.taxIdentificationNumber = taxIdentificationNumber;
+		this.identityCardNumber = identityCardNumber;
+		this.registrationNumber = registrationNumber;
+		this.photoPath = photoPath;
+	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getObservation() {
+		return observation;
+	}
+
+	public void setObservation(String observation) {
+		this.observation = observation;
+	}
+
+	public String getTaxIdentificationNumber() {
+		return taxIdentificationNumber;
+	}
+
+	public void setTaxIdentificationNumber(String taxIdentificationNumber) {
+		this.taxIdentificationNumber = taxIdentificationNumber;
+	}
+
+	public String getIdentityCardNumber() {
+		return identityCardNumber;
+	}
+
+	public void setIdentityCardNumber(String identityCardNumber) {
+		this.identityCardNumber = identityCardNumber;
+	}
+
+	public String getRegistrationNumber() {
+		return registrationNumber;
+	}
+
+	public void setRegistrationNumber(String registrationNumber) {
+		this.registrationNumber = registrationNumber;
+	}
+
+	public String getPhotoPath() {
+		return photoPath;
+	}
+
+	public void setPhotoPath(String photoPath) {
+		this.photoPath = photoPath;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Teacher other = (Teacher) obj;
+		return Objects.equals(id, other.id);
+	}
+}
